b7c823aee873c948a5e2c8ea262ec425cc6c6dab
sumit4267 <ss59gms@gmail.com>
2025-06-29 22:56:19 +0530

    first commit

--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,7 @@ const userSchema = mongoose.Schema(
     role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
     recentSearchedCities: [{ type: String, required: true }],
   },
-  { timestamps: true }
+  { timestamps: true }
 );
 
 const User = mongoose.model("User",userSchema);
