76299b8333991a6aad31c6ba5ba4040b1de309a5
sumit4267 <ss59gms@gmail.com>
2025-06-29 23:04:42 +0530

    first commit

--- server/configs/db.js
+++ server/configs/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     mongoose.connection.on('connected', () =>
       console.log("Database connected")
     );
-    await mongoose.connect(`${process.env.MONGODB_URI}/HOTEL-BOOKING`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`);
   } catch (error) {
     console.log(error.message);
   }
